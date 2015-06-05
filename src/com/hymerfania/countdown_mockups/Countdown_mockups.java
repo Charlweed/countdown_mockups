3466b1e46894672d1260af1af38d282468f4cbf1
chymes <chymes@executor.hymesruzicka.org>
2015-06-05 13:25:53 -0700

    Way over due checkin. form is broken :(

---
+++ src/com/hymerfania/countdown_mockups/Countdown_mockups.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hymerfania.countdown_mockups;
+
+/**
+ *
+ * @author chymes
+ */
+public class Countdown_mockups {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
